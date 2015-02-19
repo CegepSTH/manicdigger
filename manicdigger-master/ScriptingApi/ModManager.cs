d8656a7fbdac39f1c1943de39fd18f91a3b03480
Mathew Lemonde <mathew_lemonde@hotmail.com>
2015-02-19 10:19:25 -0500

    Comment hoe recipe

--- manicdigger-master/ScriptingApi/ModManager.cs
+++ manicdigger-master/ScriptingApi/ModManager.cs
@@ -955,47 +955,47 @@ namespace ManicDigger
             ings.Add(new CraftIngredient(154, 1, 1, 3));
             AddCraftRecipe(ings, new CraftIngredient(172, 1, 4, 2),33);
             #endregion
-            #region Hoe
-            //Recipe Hoe wood
-            ings.Clear();
-            ings.Add(new CraftIngredient(5, 1, 0, 1));
-            ings.Add(new CraftIngredient(5, 1, 1, 1));
-            ings.Add(new CraftIngredient(154, 1, 1, 2));
-            ings.Add(new CraftIngredient(154, 1, 1, 3));
-            AddCraftRecipe(ings, new CraftIngredient(169, 1, 4, 2),60);
-
-            //Recipe Hoe stone
-            ings.Clear();
-            ings.Add(new CraftIngredient(1, 1, 0, 1));
-            ings.Add(new CraftIngredient(1, 1, 1, 1));
-            ings.Add(new CraftIngredient(154, 1, 1, 2));
-            ings.Add(new CraftIngredient(154, 1, 1, 3));
-            AddCraftRecipe(ings, new CraftIngredient(168, 1, 4, 2), 132);
-
-            //Recipe Hoe iron
-            ings.Clear();
-            ings.Add(new CraftIngredient(15, 1, 0, 1));
-            ings.Add(new CraftIngredient(15, 1, 1, 1));
-            ings.Add(new CraftIngredient(154, 1, 1, 2));
-            ings.Add(new CraftIngredient(154, 1, 1, 3));
-            AddCraftRecipe(ings, new CraftIngredient(166, 1, 4, 2), 251);
-
-            //Recipe Hoe silver
-            ings.Clear();
-            ings.Add(new CraftIngredient(133, 1, 0, 1));
-            ings.Add(new CraftIngredient(133, 1, 1, 1));
-            ings.Add(new CraftIngredient(154, 1, 1, 2));
-            ings.Add(new CraftIngredient(154, 1, 1, 3));
-            AddCraftRecipe(ings, new CraftIngredient(165, 1, 4, 2), 1562);
-
-            //Recipe Hoe gold
-            ings.Clear();
-            ings.Add(new CraftIngredient(14, 1, 0, 1));
-            ings.Add(new CraftIngredient(14, 1, 1, 1));
-            ings.Add(new CraftIngredient(154, 1, 1, 2));
-            ings.Add(new CraftIngredient(154, 1, 1, 3));
-            AddCraftRecipe(ings, new CraftIngredient(167, 1, 4, 2),33);
-            #endregion
+        //  #region Hoe
+         //   //Recipe Hoe wood
+         //   ings.Clear();
+         //   ings.Add(new CraftIngredient(5, 1, 0, 1));
+         //   ings.Add(new CraftIngredient(5, 1, 1, 1));
+         //   ings.Add(new CraftIngredient(154, 1, 1, 2));
+         //   ings.Add(new CraftIngredient(154, 1, 1, 3));
+         //   AddCraftRecipe(ings, new CraftIngredient(169, 1, 4, 2),60);
+         //
+         //   //Recipe Hoe stone
+         //   ings.Clear();
+         //   ings.Add(new CraftIngredient(1, 1, 0, 1));
+         //   ings.Add(new CraftIngredient(1, 1, 1, 1));
+         //   ings.Add(new CraftIngredient(154, 1, 1, 2));
+         //   ings.Add(new CraftIngredient(154, 1, 1, 3));
+         //   AddCraftRecipe(ings, new CraftIngredient(168, 1, 4, 2), 132);
+         //
+         //   //Recipe Hoe iron
+         //   ings.Clear();
+         //   ings.Add(new CraftIngredient(15, 1, 0, 1));
+         //   ings.Add(new CraftIngredient(15, 1, 1, 1));
+         //   ings.Add(new CraftIngredient(154, 1, 1, 2));
+         //   ings.Add(new CraftIngredient(154, 1, 1, 3));
+         //   AddCraftRecipe(ings, new CraftIngredient(166, 1, 4, 2), 251);
+         //
+         //   //Recipe Hoe silver
+         //   ings.Clear();
+         //   ings.Add(new CraftIngredient(133, 1, 0, 1));
+         //   ings.Add(new CraftIngredient(133, 1, 1, 1));
+         //   ings.Add(new CraftIngredient(154, 1, 1, 2));
+         //   ings.Add(new CraftIngredient(154, 1, 1, 3));
+         //   AddCraftRecipe(ings, new CraftIngredient(165, 1, 4, 2), 1562);
+         //
+         //   //Recipe Hoe gold
+         //   ings.Clear();
+         //   ings.Add(new CraftIngredient(14, 1, 0, 1));
+         //   ings.Add(new CraftIngredient(14, 1, 1, 1));
+         //   ings.Add(new CraftIngredient(154, 1, 1, 2));
+         //   ings.Add(new CraftIngredient(154, 1, 1, 3));
+         //   AddCraftRecipe(ings, new CraftIngredient(167, 1, 4, 2),33);
+         //   #endregion
 
             #region Helm
             //Wood helm
